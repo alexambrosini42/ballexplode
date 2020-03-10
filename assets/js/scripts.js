51bdaa2c82d3ccd6ea051841b9351536e14a8b7c
Alex Ambrosini <alex.28064212@gmail.com>
2020-03-10 20:06:03 -0300

    Primeiro save, criação do melhor joguinho

---
+++ assets/js/scripts.js
@@ -0,0 +1,18 @@
+function addBall(){
+    var ball = document.createElement('div');
+    ball.setAttribute('class', 'ball');
+    var p1 = Math.floor(Math.random() * (document.documentElement.clientHeight -140 ));
+    var p2 = Math.floor(Math.random() * (document.documentElement.clientWidth -140 ));
+    var rdm = Math.floor(Math.random() * 899);
+    ball.innerHTML = "Clique aqui!";
+    ball.setAttribute('style','left:'+p2+'px; top:'+p1+'px; background-color:#'+rdm);
+    ball.setAttribute('onclick','addBall();pop(this)');
+    document.body.appendChild(ball);
+}
+function pop(ball){
+    document.body.removeChild(ball);
+}
+function playGame(){
+    addBall()
+   // setInterval(addBall, 500);
+}
