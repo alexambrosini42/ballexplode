e417393528cd70046743dc99df0fa01f83f7d3be
Alex Ambrosini <alex.28064212@gmail.com>
2020-03-10 21:55:11 -0300

    Adição de animação bounce modificação do js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,8 @@
 function addBall(){
     var ball = document.createElement('div');
     ball.setAttribute('class', 'ball');
-    var p1 = Math.floor(Math.random() * (document.documentElement.clientHeight -140 ));
-    var p2 = Math.floor(Math.random() * (document.documentElement.clientWidth -140 ));
+    var p1 = Math.floor(Math.random() * (document.documentElement.clientHeight - 132 ));
+    var p2 = Math.floor(Math.random() * (document.documentElement.clientWidth - 132 ));
     var rdm = Math.floor(Math.random() * 899);
     ball.innerHTML = "Clique aqui!";
     ball.setAttribute('style','left:'+p2+'px; top:'+p1+'px; background-color:#'+rdm);
@@ -14,5 +14,5 @@ function pop(ball){
 }
 function playGame(){
     addBall()
-   // setInterval(addBall, 500);
+  // setInterval(addBall, 2000);
 }
