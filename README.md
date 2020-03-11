8bc934734375fc296dab51eb7e4f144c20e4c0c0
Alex Ambrosini <alex.28064212@gmail.com>
2020-03-10 21:09:40 -0300

    Alterando README e Title index

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ballexplode
-Explodir balões estilizados usando javascript nativo
+
+Explodir balões estilizados usando javascript nativo, esse é um teste de utilização de GitHub.
+
+Caso queira complementar esse codigo, treinando também envio de correção, fique a vontade!
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jogo de estourar bola</title>
+    <title>The best ball game ever</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <script src="assets/js/scripts.js"></script>
 </head>
